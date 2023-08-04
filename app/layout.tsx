d80d593da87453490d9bec624edcd22b888834c2
fvasquezl <vasquez.limon@gmail.com>
2023-08-03 22:52:59 -0700

    context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import AuthProvider from './context/AuthProvider'
+import NavBar from './components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>
+          <NavBar />
+          <main className='flex justify-centet items-start p-6 min-h-screen'>
+            {children}
+          </main>
+        </AuthProvider>
+
+      </body>
     </html>
   )
 }
