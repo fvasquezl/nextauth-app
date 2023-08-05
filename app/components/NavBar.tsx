3d7b1d8b35fe4f71d3d6725ff867f857c5769cb0
fvasquezl <vasquez.limon@gmail.com>
2023-08-04 21:41:20 -0700

    complete

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link"
 
-
-const NavBar = () => {
+export default function Navbar() {
     return (
-        <div>NavBar</div>
+        <nav className="bg-blue-800 p-4">
+            <ul className="flex justify-evenly text-2xl font-bold">
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/api/auth/signin">Sign In</Link></li>
+                <li><Link href="/api/auth/signout">Sign Out</Link></li>
+                <li><Link href="/server">Server</Link></li>
+                <li><Link href="/client">Client</Link></li>
+                <li><Link href="/extra">Extra</Link></li>
+            </ul>
+        </nav>
     )
 }
-
-export default NavBar
